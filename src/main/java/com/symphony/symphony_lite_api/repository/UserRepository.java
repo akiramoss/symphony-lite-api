d906f9df40509f93915e17b05bcc94461a45ae9f
Iñaki Ramos <iramositurria@gmail.com>
2026-04-06 16:33:13 +0200

    feat: add user repository with custom queries

--- src/main/java/com/symphony/symphony_lite_api/repository/UserRepository.java
+++ src/main/java/com/symphony/symphony_lite_api/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.symphony.symphony_lite_api.repository;
 import com.symphony.symphony_lite_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
