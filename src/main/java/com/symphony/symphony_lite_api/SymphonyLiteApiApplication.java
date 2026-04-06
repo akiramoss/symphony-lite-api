3dc6e673802d4f41730034aaeb8e16e89c97ccc0
Iñaki Ramos <iramositurria@gmail.com>
2026-04-06 15:24:42 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/symphony/symphony_lite_api/SymphonyLiteApiApplication.java
@@ -0,0 +1,12 @@
+package com.symphony.symphony_lite_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymphonyLiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymphonyLiteApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/symphony/symphony_lite_api/SymphonyLiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symphony.symphony_lite_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymphonyLiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
