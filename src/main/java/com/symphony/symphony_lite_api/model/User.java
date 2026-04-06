55946d93ab2c4271977fde9b350c1ce571055bb5
Iñaki Ramos <iramositurria@gmail.com>
2026-04-06 16:29:21 +0200

    feat: create User entity with constraints

--- src/main/java/com/symphony/symphony_lite_api/model/User.java
+++ src/main/java/com/symphony/symphony_lite_api/model/User.java
@@ -1,20 +1,24 @@
 package com.symphony.symphony_lite_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Data
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
-    private String email;
 
+    @Column(nullable = false, unique = true)
+    private String email;
 }
