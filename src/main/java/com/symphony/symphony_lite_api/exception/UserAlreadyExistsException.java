f31dd5510bbefb90a9ff7fc3c561d69c7ab3763b
Iñaki Ramos <iramositurria@gmail.com>
2026-04-07 19:18:17 +0200

    feat: add UserAreadyExsistsException

---
+++ src/main/java/com/symphony/symphony_lite_api/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.symphony.symphony_lite_api.exception;
+
+public class UserAlreadyExistsException extends ApiException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
