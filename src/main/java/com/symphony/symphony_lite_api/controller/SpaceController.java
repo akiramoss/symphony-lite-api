84c8f809f80bc68fb97563767e2847da79dbf2fa
Iñaki Ramos <iramositurria@gmail.com>
2026-04-09 14:35:54 +0200

    fix: change joinSpace method in SpaceController to specify http path

--- src/main/java/com/symphony/symphony_lite_api/controller/SpaceController.java
+++ src/main/java/com/symphony/symphony_lite_api/controller/SpaceController.java
@@ -1,7 +1,5 @@
 package com.symphony.symphony_lite_api.controller;
 
-import com.symphony.symphony_lite_api.dto.CreateSpaceRequest;
-import com.symphony.symphony_lite_api.dto.SpaceResponse;
 import com.symphony.symphony_lite_api.service.SpaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +11,11 @@ public class SpaceController {
 
     private final SpaceService spaceService;
 
-    @PostMapping
-    public SpaceResponse createSpace(@RequestBody CreateSpaceRequest createSpaceRequest) {
-        return spaceService.createSpace(createSpaceRequest);
+    @PostMapping("/{spaceId}/join/{userId}")
+    public String joinSpace(@PathVariable Long spaceId, @PathVariable Long userId) {
+
+        spaceService.joinSpace(spaceId, userId);
+
+        return "User joined space successfully";
     }
 }
