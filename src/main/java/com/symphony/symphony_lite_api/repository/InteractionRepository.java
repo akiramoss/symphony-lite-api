72433a2bf230aefb38874eabe6ebad0da7a9d75e
Iñaki Ramos <iramositurria@gmail.com>
2026-04-09 14:20:14 +0200

    feat: add interaction repository with query methods

---
+++ src/main/java/com/symphony/symphony_lite_api/repository/InteractionRepository.java
@@ -0,0 +1,14 @@
+package com.symphony.symphony_lite_api.repository;
+
+import com.symphony.symphony_lite_api.model.Interaction;
+import com.symphony.symphony_lite_api.model.InteractionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InteractionRepository extends JpaRepository<Interaction, Long> {
+
+    List<Interaction> findByUserId(Long userId);
+
+    List<Interaction> findBySpaceId(Long spaceId);
+}
