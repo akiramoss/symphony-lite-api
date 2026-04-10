8f7361ebd217c378cc5c0ed523f57e4be4c17752
Iñaki Ramos <iramositurria@gmail.com>
2026-04-10 09:48:26 +0200

    feat: add AnalyticsRepository to use projection instead of a raw Object[]

---
+++ src/main/java/com/symphony/symphony_lite_api/repository/AnalyticsRepository.java
@@ -0,0 +1,20 @@
+package com.symphony.symphony_lite_api.repository;
+
+import com.symphony.symphony_lite_api.model.Interaction;
+import com.symphony.symphony_lite_api.projection.SpaceCountProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AnalyticsRepository extends JpaRepository<Interaction, Long> {
+
+    @Query("""
+                SELECT i.spaceId as spaceId, COUNT(i) as count
+                FROM Interaction i
+                WHERE i.type = 'JOIN'
+                GROUP BY i.spaceId
+                ORDER BY count DESC
+            """)
+    List<SpaceCountProjection> findTopSpaces();
+}
