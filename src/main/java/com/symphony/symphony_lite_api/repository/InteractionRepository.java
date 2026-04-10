eb1a9168b174c14728557eac1e2dabf79a867c4a
Iñaki Ramos <iramositurria@gmail.com>
2026-04-10 09:32:52 +0200

    feat: add query to get top spaces by join interactions

--- src/main/java/com/symphony/symphony_lite_api/repository/InteractionRepository.java
+++ src/main/java/com/symphony/symphony_lite_api/repository/InteractionRepository.java
@@ -3,6 +3,7 @@ package com.symphony.symphony_lite_api.repository;
 import com.symphony.symphony_lite_api.model.Interaction;
 import com.symphony.symphony_lite_api.model.InteractionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,13 @@ public interface InteractionRepository extends JpaRepository<Interaction, Long> 
     List<Interaction> findByUserId(Long userId);
 
     List<Interaction> findBySpaceId(Long spaceId);
+
+    @Query("""
+            SELECT i.spaceId, COUNT(i) as count
+            FROM Interaction i
+            WHERE i.type = 'JOIN'
+            GROUP BY i.spaceId
+            ORDER BY count DESC
+            """)
+    List<Object[]> findTopSpaces();
 }
